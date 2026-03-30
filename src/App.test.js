7822b6ffa700f30f819915266997e1df3b33fe27
justine <jusyanong@gmail.com>
2026-03-30 12:53:44 +0800

    Edit .html

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders landing page', () => {
-  render(<App />);
-  const heading = screen.getByText(/fearlessly/i);
-  expect(heading).toBeInTheDocument();
-});
